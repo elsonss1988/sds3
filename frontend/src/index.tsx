6f5f94bbce7534d9a7065f88fe703ae35b654ae4
elsonss1988 <elsonss1988@gmail.com>
2021-05-05 14:49:42 -0300

    Basic static components

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'assets/css/styles.css';
+import Nav from 'components/NavBar';
 
 ReactDOM.render(
   <React.StrictMode>
