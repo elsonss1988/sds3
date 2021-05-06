3f4cf3dd0f86e63a3f59004ac49dea7e219e1a78
elsonss1988 <elsonss1988@gmail.com>
2021-05-06 09:34:54 -0300

    removendo NAV

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'assets/css/styles.css';
-import Nav from 'components/NavBar';
 
 ReactDOM.render(
   <React.StrictMode>
