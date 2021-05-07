b5ab3b635626d0a1edb4bb71e0b0a61711bea0e4
elsonss1988 <elsonss1988@gmail.com>
2021-05-07 09:08:19 -0300

    Layers

---
+++ backend/src/main/java/com/example/dsvendas/Repositories/SellerRepository.java
@@ -0,0 +1,7 @@
+package com.example.dsvendas.Repositories;
+
+import com.example.dsvendas.entities.Seller;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SellerRepository extends JpaRepository<Seller,Long> {
+}
