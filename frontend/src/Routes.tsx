c0afef374f0319529fb65249dd8a4308974e4c08
elsonss1988 <elsonss1988@gmail.com>
2021-05-09 13:16:55 -0300

    Env var

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -1,6 +1,5 @@
 import Dashboard from 'pages/Dashboard';
 import Home from 'pages/Home';
-import React from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
 const Routes=()=>{
     return(
